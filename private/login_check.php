3c713b947faa7de1352f18d25b0ab575eeefa61d
bojan <bojan@bisaudiosk.local>
2018-12-17 19:08:35 +0100

    login check.strana3

--- private/login_check.php
+++ private/login_check.php
@@ -1,4 +1,29 @@
 <?php
 
-require dirname(__FILE__)."/../framework/helpers.php";
-$_POST
+require_once dirname(__FILE__)."/../framework/helpers.php";
+
+if (!empty($_POST)){
+    if(!empty($_POST['email'] &&['password'])){
+        // toto su data pre ulohu 4 zo 17.12
+        $users =db_select(sprintf("SELECT * FROM Users WHERE email = '%s'", $_POST['email']) );
+       
+        if(!empty($users[0])){
+            $user=b$users[0];
+            if($user-> passowrd ==$_POST['password']){
+                echo  $user->email;
+            }else{
+                //echo "Nesuhlasi heslo"
+                echo $user->email;
+            }else{
+                //"nenasiel sa pouzivatel"
+
+            }
+        }
+    
+    }
+
+};
+
+$result=db_select(Select *from 'Users' );
+
+?>
