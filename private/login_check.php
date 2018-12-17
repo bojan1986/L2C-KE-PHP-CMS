936bd2305acc0948d97359a0648a154b0264d855
bojan <bojan@bisaudiosk.local>
2018-12-17 19:40:22 +0100

    strana3, uloha4

--- private/login_check.php
+++ private/login_check.php
@@ -3,27 +3,29 @@
 require_once dirname(__FILE__)."/../framework/helpers.php";
 
 if (!empty($_POST)){
-    if(!empty($_POST['email'] &&['password'])){
+    if(!empty($_POST['email'] && !empty($_POST['password'])){
         // toto su data pre ulohu 4 zo 17.12
         $users =db_select(sprintf("SELECT * FROM Users WHERE email = '%s'", $_POST['email']) );
        
-        if(!empty($users[0])){
-            $user=b$users[0];
-            if($user-> passowrd ==$_POST['password']){
+        if(!empty($users)){
+            $user=$users[0];
+
+            if($user->password == $_POST['password']){
+            //email aj heslo suhlasia, ideme dalej
                 echo  $user->email;
             }else{
-                //echo "Nesuhlasi heslo"
-                echo $user->email;
-            }else{
-                //"nenasiel sa pouzivatel"
-
+                echo "Nesuhlasi heslo";
+                $_POST['email']&&['password'];
+              //echo "Nesuhlasi heslo"
             }
+           
         }
-    
+        else{
+            //"nenasiel sa pouzivatel"
+            echo "neznamy pouzivatel";
+           
+        }
     }
 
 };
-
-$result=db_select(Select *from 'Users' );
-
 ?>
